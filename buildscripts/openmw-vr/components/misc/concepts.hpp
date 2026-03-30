e3acbc3df9a30e4864e6c2fcd720d62d1b2812cb
mmry2940 <mmry2940@gmail.com>
2026-03-29 23:19:18 -0400

    fix(build): add LuaJIT 2.1.0-beta3 + SQLite3 3.39.4 Android cross-compile
    - Add ExternalProject_Add(luajit): GitHub URL, CC=clang (bare name, CROSS
      handles prefix since STATIC_CC = $(CROSS)$(CC) in LuaJIT Makefile),
      HOST_CC=gcc, TARGET_SYS=Linux, BUILDMODE=static
    - Add ExternalProject_Add(sqlite3): autoconf build with COMMON_AUTOCONF_FLAGS
    - Wire both into openmw DEPENDS and OPENMW_COMMON hint paths:
      LuaJit_INCLUDE_DIR, LuaJit_LIBRARY, SQLite3_INCLUDE_DIR, SQLite3_LIBRARY
    - concepts.hpp: replace std::same_as+\<concepts\> with std::is_same_v+\<type_traits\>
      (std::same_as requires \<concepts\> header absent in NDK r21 libc++)

--- buildscripts/openmw-vr/components/misc/concepts.hpp
+++ buildscripts/openmw-vr/components/misc/concepts.hpp
@@ -1,13 +1,14 @@
 #ifndef OPENMW_COMPONENTS_MISC_CONCEPTS_H
 #define OPENMW_COMPONENTS_MISC_CONCEPTS_H
 
-#include <concepts>
 #include <type_traits>
 
 namespace Misc
 {
+    // Note: std::same_as from <concepts> unavailable with NDK r21/Clang 9;
+    // semantically equivalent replacement using <type_traits>.
     template <class T, class U>
-    concept SameAsWithoutCvref = std::same_as<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
+    concept SameAsWithoutCvref = std::is_same_v<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 }
 
 #endif
